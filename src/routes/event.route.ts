8d890f7a59ee8f524dfadc1a5d07276212b5e04b
regagiya <regagiya@gmail.com>
2025-10-27 17:06:07 +0700

    update route

--- src/routes/event.route.ts
+++ src/routes/event.route.ts
@@ -9,7 +9,7 @@ import {
 
 const router = express.Router();
 
-router.get("/", getAllEvents);
+router.get("/events", getAllEvents);
 router.get("/:id", getEventById);
 router.post("/create", createEvent);
 router.put("/:id", updateEvent);
